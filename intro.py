4958e99019503b3b9c12db0760baf8fd210eb3b4
niranjan122005 <niranjanv@thestackly.com>
2026-08-25 11:52:06 +0530

    Intro

---
+++ intro.py
@@ -0,0 +1,8 @@
+'''these are printing statements
+these are printing statements
+these are printing statements
+these are printing statements
+these are printing statements'''
+print(3)
+print(4)
+print(5)
