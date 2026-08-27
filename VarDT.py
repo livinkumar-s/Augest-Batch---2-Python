e362c5753a02f9b2b286b806df2452ece289cb94
niranjan122005 <niranjanv@thestackly.com>
2026-08-27 11:31:59 +0530

    Variables, Datatypes and Type casting

---
+++ VarDT.py
@@ -0,0 +1,53 @@
+# c1=10 #create
+# Print=40
+# # print(Print)
+# c1=15 #reassign
+# print(c1)
+
+# a=-10.4567
+# print(type(a))
+# print(a)
+
+# a=10
+# b=5.5
+
+# print(a+b)
+
+# a='I said "Leo"'
+
+# print(a)
+# print(type(a))
+
+# str1="Hello"
+# str2="World!"
+
+# print(str1 + str2 + "....")
+# print(4 + 6.4) #10
+
+# print("Hell"+10) #error
+
+# name="Ken"
+# age=66
+
+# His name is Ken. He is 66.
+
+# print("His name is "+name+". He is "+str(age)+".")
+
+# a="777"
+# print(a+100)
+
+# b=False 
+# print(b)
+
+#data --> will be shown at the output(code to output)
+#input --> (output to the code)
+
+# a=input("Enter Your Name: ") # "55"
+
+# print(type(a))
+# print(a)
+# print(a)
+# print(a)
+
+inp=int(input("Enter a Number: "))
+print(inp+100)
