63d8c57cdb0f3974d4fffd541e00c39781ae53c7
niranjan122005 <niranjanv@thestackly.com>
2026-09-15 13:56:56 +0530

    Functions

---
+++ Functions.py
@@ -0,0 +1,75 @@
+# def printSteps():
+#     print("Step1")
+#     print("Step2")
+#     print("Step3")
+#     print("Step4")
+#     print("Step5")
+#     print("----------------")
+
+# a=printSteps() #caller
+# print(a)
+# printSteps()
+# printSteps()
+# printSteps()
+# printSteps()
+# printSteps()
+
+# def addNum(a,b):
+#     print(a)
+#     print(b)
+#     print(a+b)
+
+# addNum(1,2)
+# addNum(5,3)
+
+# def addNums(a,b,c=10):
+#     print(a+b+c)
+
+# addNums(3,4)
+
+# addNums(c=1,a=2,b=3)
+# a=int(input("Enter 1st Numb: "))
+# b=int(input("Enter 2nt Numb: "))
+# c=int(input("Enter 3rd Numb: "))
+# addNums(int(input("Enter 1st Numb: ")),int(input("Enter 2nt Numb: ")),int(input("Enter 3rd Numb: ")))
+# def greet(n1,n2):
+#     print("Hello "+n1+". From "+n2+".")
+
+# greet("Leo",12)
+
+
+# def addNums(*a):
+#     ans=0
+#     for i in a:
+#         ans+=i
+#     print(ans)
+
+# addNums(5,6,7)
+
+# def getDetail(**a):
+#     print(a)
+
+
+# getDetail(name="Leo",age=23,role="FED")
+
+
+# def add(a,b):
+#     print(a+b)
+
+# print(add(2,2))
+
+# def mul(a,b):
+#     return a*b 
+
+# print(mul(2,mul(3,mul(4,mul(7,mul(2,-1))))))
+
+
+# print()
+# input()
+# int()
+# len()
+# type()
+
+# print(1,2,3,4,5,6,7,8)
+# a=input("Enter name: ")
+# print(a)
