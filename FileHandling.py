383c457f73c9ddece6d252b419fec1a75386c9f0
niranjan122005 <niranjanv@thestackly.com>
2026-09-23 11:12:21 +0530

    File Handling

---
+++ FileHandling.py
@@ -0,0 +1,31 @@
+# file=open("text1.txt","r")
+# print(file)
+# content=file.read(15)
+# file.seek(0)
+# content=file.read(5)
+# print(content)
+# print(file.tell())
+
+# cont=file.readline()
+# cont=file.readline()
+# cont=file.readline()
+# print(cont)
+# cont=file.readlines()[9]
+# print(cont)
+# file.close()
+
+# file=open("text.txt","w")
+# # print(file.writable())
+# file.write("Hello\nHi\nWelcome")
+# file.close()
+
+# file=open("text.txt","a")
+# # print(file.writable())
+# file.write("\nHello\nHi\nWelcome")
+# file.close()
+
+# with open("text.txt") as f:
+#     print(f.readable())
+#     print(f.read())
+
+# print(f)
