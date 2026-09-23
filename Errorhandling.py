4ae4cd99969ec623f4ef51485962d6c18e6ee92e
niranjan122005 <niranjanv@thestackly.com>
2026-09-23 11:35:21 +0530

    Error Handling

---
+++ Errorhandling.py
@@ -0,0 +1,41 @@
+# if True:
+# print(1)
+# print(2)
+# print(3)
+
+print(1)
+print(2)
+print(3)
+
+# print("Hello)
+# print("Hello"+12)
+# print(int("Hello"))
+
+# try:
+#     print(int("Hi"))
+#     print(4/0)
+#     print("Hi")
+#     print("Hello")
+# except ZeroDivisionError:
+#     print("You cannot divide that number by 0")
+# except ValueError:
+#     print("Invalid Conversion")
+
+# try:
+#     num1=int(input("Enter Your First Number: "))
+#     num2=int(input("Enter Your Second Number: "))
+#     print(num1/num2)
+# except ValueError:
+#     print("Invalid Input!")
+# except ZeroDivisionError:
+#     print("You cannot divide that number by 0")
+# finally:
+#     print("Done")
+
+
+# print(10)
+
+i=1
+
+while i<=10:
+    print(i)
